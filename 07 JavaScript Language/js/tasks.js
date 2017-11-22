4ffbc4d1ddfa1f2313c4c849d3647560e57b64ad
Unknown <joni.adamsson@gmail.com>
2017-11-22 08:51:14 +0200

    fixes

--- 07 JavaScript Language/js/tasks.js
+++ 07 JavaScript Language/js/tasks.js
@@ -2,10 +2,30 @@
 // This script manages a to-do list.
 
 // Need a global variable:
-var tasks = []; 
+var tasks = [];
 
 // Function called when the form is submitted.
 // Function adds a task to the global array.
+function removeduplicates() {
+  'use strict';
+    var result = [];
+    for(i=0;i<tasks.length;i++){
+      if(result.indexOf(task[i]) == -1){
+        result.push(task[i]);
+      }
+    }
+    message = '<h2>To-Do</h2><ol>';
+    for (var i = 0, count = tasks.length; i < count; i++) {
+        message += '<li>' + tasks[i] + '</li>';
+    }
+    message += '</ol>';
+    output.innerHTML = message;
+
+} // End of task.value IF.
+
+// Return false to prevent submission:
+return false;
+}
 function addTask() {
     'use strict';
 
@@ -14,15 +34,15 @@ function addTask() {
 
     // Reference to where the output goes:
     var output = document.getElementById('output');
-    
+
     // For the output:
     var message = '';
 
     if (task.value) {
-    
+
         // Add the item to the array:
         tasks.push(task.value);
-        
+
         // Update the page:
         message = '<h2>To-Do</h2><ol>';
         for (var i = 0, count = tasks.length; i < count; i++) {
@@ -30,27 +50,16 @@ function addTask() {
         }
         message += '</ol>';
         output.innerHTML = message;
-        
+
     } // End of task.value IF.
 
     // Return false to prevent submission:
     return false;
-    
-} // End of addTask() function.
-function removeduplicates() {
-	'use strict';
-	var alt = tasks
-	.map((task)=>{
-		return {count: 1, task: task}
-	})
-	.reduce((a, b)=> {
-		a[b.task] = (a[b.name] || 0) + b.count
-		return a},{})
-	var duplicates = Object.keys(alt).filter((a)=> uniq[a]>1
-	
-	}
 
-	
+} // End of addTask() function.
+
+
+
 // Initial setup:
 function init() {
 	'use strict';
