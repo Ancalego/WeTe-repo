105890b9a2e922017b4fccdd0db2e8d376a97df0
Unknown <joni.adamsson@gmail.com>
2017-12-15 16:32:22 +0200

    All exercises

--- 06 JavaScript Tools/js/notes.js
+++ 06 JavaScript Tools/js/notes.js
@@ -1,44 +1,63 @@
-
+'use strict';
 var notes = new Array();
+loadList();
 
-function addItem() {
-	textbox = document.getElementById('item');
-	var itemText = textbox.value;
-	textbox.value = '';
-	textbox.focus();
-	var newItem = {title: itemText, quantity: 1};
-	notes.push(newItem);
-	displayList();
+function getItemText(){
+    var textbox = document.getElementById('item');
+    var itemText = textbox.value;
+    textbox.value = '';
+    textbox.focus();
+    return itemText;
 }
 
-function displayList() {
-	var table = document.getElementById('list');
-	table.innerHTML = '';
-	for (var i = 0; i<notes.length; i++) {
-		var node = undefined;
-		var note = notes[i];
-		var node = document.createElement('tr');
-		var html = '<td>'+note.title+'</td><td>'+note.quantity+'</td><td><a href="#" onClick="deleteIndex('+i+')">delete</td>';
-	    node.innerHTML = html;
-		table.appendChild(node);
-	}
+function addItem(){
+    var itemText = getItemText();
+    var newItem = {title: itemText, quantity: 1};
+    var duplicate = 0;
+
+    notes.forEach(function(el){
+        if (el.title == itemText){
+            el.quantity++;
+            duplicate = 1;
+        }
+    });
+    if (duplicate == 0){
+        notes.push(newItem);
+    }
+    displayList();
+    saveList();
 }
 
-function deleteIndex(i) {
-	notes.splice(i, 1);
-	displayList();
+function displayList(){
+    var table = document.getElementById('list');
+    table.innerHTML = '';
+    for (var i = 0; i<notes.length; i++){
+        var node = undefined;
+        var note = notes[i];
+        node = document.createElement('tr');
+        var html = '<td>'+note.title+'</td><td>'+note.quantity+'</td><td><a href="#" onClick="deleteIndex('+i+')">delete</td>';
+        node.innerHTML = html;
+        table.appendChild(node);
+    }
 }
-function saveList() {
+
+function deleteIndex(i){
+    notes.splice(i, 1);
+    displayList();
+    saveList();
+}
+
+function saveList(){
     localStorage.notes = JSON.stringify(notes);
 }
 
-function loadList() {
+function loadList(){
     console.log('loadList');
-    if (localStorage.notes) {
+    if (localStorage.notes){
         notes = JSON.parse(localStorage.notes);
         displayList();
     }
 }
 
-button = document.getElementById('add');
+var button = document.getElementById('add');
 button.onclick = addItem;
--- 06 JavaScript Tools/js/shopping.js
+++ 06 JavaScript Tools/js/shopping.js
@@ -4,59 +4,61 @@
 // Function called when the form is submitted.
 // Function performs the calculation and returns false.
 function calculate() {
-	'use strict';
+  'use strict';
 
-	// For storing the order total:
-	var total;
+  // For storing the order total:
+  var total;
 
-	// Get references to the form values:
-	var quantity = parseInt(document.getElementById('quantity').value);
-	var price = parsefloat(document.getElementById('price').value);
-	var tax = parsefloat(document.getElementById('tax').value);
-	var discount = parsefloat(document.getElementById('discount').value);
-	var shipping = parsefloat(document.getElementById('shipping').value);
-	// Add validation here later!
+  // Get references to the form values:
+  var quantity = parseInt(document.getElementById('quantity').value);
+  var price = parseFloat(document.getElementById('price').value);
+  var tax = parseFloat(document.getElementById('tax').value);
+  var discount = parseFloat(document.getElementById('discount').value);
+  var shipping = parseFloat(document.getElementById('shipping').value);
 
-	// Calculate the initial total:
-	total = quantity * price;
-	console.log("total before tax: " + total);
+  // Add validation here later!
 
-	// Make the tax rate easier to use:
-	tax = tax / 100;
-	tax = tax + 1;
+  // Calculate the initial total:
 
-	// Factor in the tax:
-	total = total * tax;
-	console.log("total after tax: " + total);
 
-	// Factor in the discount:
-	if (quantity>100){
-	discount = discount*2;
-	}
+  if(quantity > 100){
+    discount = discount * 2;
+  }
+  total = quantity * price;
+  console.log("total before tax: " + total);
 
-	total = total -discount;
-	total = total + shipping;
-	console.log("total after discount: " + total+discount);
+  // Make the tax rate easier to use:
+  tax = tax / 100;
+  tax = tax + 1;
 
-	// Format the total to two decimal places:
-	total = total.toFixed(2);
+  // Factor in the tax:
+  total = total * tax;
+  console.log("total after tax: " + total);
 
-	// Display the total:
-	document.getElementById('total').value = total;
+  // Factor in the discount:
+  total = total - discount;
+  console.log("total after discount: " + total);
 
-	// Return false to prevent submission:
-	return false;
+  total = total + shipping;
+
+  // Format the total to two decimal places:
+  total = total.toFixed(2);
+
+  // Display the total:
+  document.getElementById('total').value = total;
+  // Return false to prevent submission:
+  return false;
 
 } // End of calculate() function.
 
 // Function called when the window has been loaded.
 // Function needs to add an event listener to the form.
 function init() {
-	'use strict';
+  'use strict';
 
-	// Add an event listener to the form:
-	var theForm = document.getElementById('theForm');
-	theForm.onsubmit = calculate;
+  // Add an event listener to the form:
+  var theForm = document.getElementById('theForm');
+  theForm.onsubmit = calculate;
 
 } // End of init() function.
 
